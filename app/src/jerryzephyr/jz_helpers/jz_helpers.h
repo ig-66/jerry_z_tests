63cdcfa85591d17d5bc7d53f6a39dd816ee62f3f
ig-66 <igor@moduhub.com>
2023-01-06 13:48:27 -0300

    Changed the helpers definition notes, to make them more informative

--- app/src/jerryzephyr/jz_helpers/jz_helpers.h
+++ app/src/jerryzephyr/jz_helpers/jz_helpers.h
@@ -3,28 +3,30 @@
 
 #include "jerryscript.h"
 
-/*  Create a new Object in the global realm
+/**  Create a new Object in the global realm
  *
- *  @param obj_name_p - char array
+ *  @param obj_name_p string name of the new object, e.g. "console" 
  *
  * @return 0 if successful, otherwise failed
  */
 int jz_object_create(char *obj_name_p);
 
-/*  Add a property or method to an existing object, should be called after jz_object_create
+/**  Add a property or method to an existing object, should be called after jz_object_create
  *
- *  @param obj_name_p - char array
- *  @param prop_name_p - char array
- *  @param prop_handler_p - external function handler
+ *  @param obj_name_p string name of the object, e.g. "console"
+ *  @param prop_name_p string name of the new prop, e.g. "log"
+ *  @param prop_handler_p external function handler, e.g. jerryx_handler_print
+ *
+ *  @note Call jz_object_create function before this one
  *
  * @return 0 if successful, otherwise failed
  */
 int jz_object_add_prop(char *obj_name_p, char *prop_name_p, jerry_external_handler_t prop_handler_p);
 
-/*  Create a new function for the JerryScript machine
+/**  Create a new function for the JerryScript machine
  *
- *  @param func_name_p - name of the new function
- *  @param func_handler_p - external function handler
+ *  @param func_name_p string name of the new function, e.g. "atob"
+ *  @param func_handler_p external function handler, e.g. atob_handler
  * 
  * @return 0 if successful, otherwise failed
  */
