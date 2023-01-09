41da4c1fcb4c7731f349986f50e5b8cfebc26b63
ig-66 <igor@moduhub.com>
2023-01-09 11:29:56 -0300

    Added and modified helpers to make object with methods inside of object

--- app/src/jerryzephyr/jz_helpers/jz_helpers.c
+++ app/src/jerryzephyr/jz_helpers/jz_helpers.c
@@ -36,18 +36,104 @@ int jz_object_add_prop(char *obj_name_p, char *prop_name_p, jerry_external_handl
     if(jerry_value_is_error(obj_ref)){
         error = -1;
     } else {
-        
+
         jerry_value_t prop_string = jerry_string_sz(prop_name_p);
         jerry_value_t prop_handler = jerry_function_external(prop_handler_p);
-        jerry_value_t set_new_prop_res = jerry_object_set(obj_ref, prop_string, prop_handler);
 
-        if (jerry_value_is_exception(set_new_prop_res)) {
-            error = -1;
+        if (jerry_value_is_function(prop_handler)) {
+            jerry_value_t set_new_prop_res = jerry_object_set(obj_ref, prop_string, prop_handler);
+
+            if (jerry_value_is_exception(set_new_prop_res)) {
+                error = -1;
+            }
+            jerry_value_free(set_new_prop_res);
+
+        } else {
+            jerry_value_t prop_obj = jerry_object();
+            jerry_value_t set_new_prop_res = jerry_object_set(obj_ref, prop_string, prop_obj);
+            
+            if (jerry_value_is_exception(set_new_prop_res)) {
+                error = -1;
+            }
+            jerry_value_free(set_new_prop_res);
+            jerry_value_free(prop_obj);
         }
 
         jerry_value_free(prop_string);
         jerry_value_free(prop_handler);
-        jerry_value_free(set_new_prop_res);
+    }
+
+    jerry_value_free(global_obj);
+    jerry_value_free(obj_name);
+    jerry_value_free(obj_ref);
+
+    return error;
+}
+
+
+int jz_object_add_obj(char *obj_name_p, char *inner_obj_name_p){
+    int error = 0;
+
+    jerry_value_t global_obj = jerry_current_realm();
+    jerry_value_t obj_name = jerry_string_sz(obj_name_p);
+    jerry_value_t obj_ref = jerry_object_get(global_obj, obj_name);
+
+    if (jerry_value_is_error(obj_ref)) {
+        error = -1;
+    } else {
+
+        jerry_value_t inner_obj = jerry_object();
+        jerry_value_t inner_obj_string = jerry_string_sz(inner_obj_name_p);
+        jerry_value_t inner_obj_set_res = jerry_object_set(obj_ref, inner_obj_string, inner_obj);
+
+        if (jerry_value_is_exception(inner_obj_set_res)) {
+            error = -1;
+        }
+
+        jerry_value_free(inner_obj_set_res);
+        jerry_value_free(inner_obj_string);
+        jerry_value_free(inner_obj);
+    }
+
+    jerry_value_free(global_obj);
+    jerry_value_free(obj_name);
+    jerry_value_free(obj_ref);
+
+    return error;
+}
+
+
+int jz_object_prop_add_prop(char *obj_name_p, char *prop_name_p, char *new_prop_name_p, jerry_external_handler_t prop_handler_p){
+    int error = 0;
+
+    jerry_value_t global_obj = jerry_current_realm();
+    jerry_value_t obj_name = jerry_string_sz(obj_name_p);
+    jerry_value_t obj_ref = jerry_object_get(global_obj, obj_name);
+
+    if(jerry_value_is_error(obj_ref)){
+        error = -1;
+    } else {
+        jerry_value_t prop_name = jerry_string_sz(prop_name_p);
+        jerry_value_t obj_prop_ref = jerry_object_get(obj_ref, prop_name);
+
+        if (jerry_value_is_error(obj_prop_ref)) {
+            error = -1;
+        } else {
+            jerry_value_t new_prop_name = jerry_string_sz(new_prop_name_p);
+            jerry_value_t new_prop_handler = jerry_function_external(prop_handler_p);
+            jerry_value_t new_prop_ref = jerry_object_set(obj_prop_ref, new_prop_name, new_prop_handler);
+
+            if(jerry_value_is_exception(new_prop_ref)){
+                error = -1;
+            }
+
+            jerry_value_free(new_prop_name);
+            jerry_value_free(new_prop_handler);
+            jerry_value_free(new_prop_ref);
+        }
+
+        jerry_value_free(prop_name);
+        jerry_value_free(obj_prop_ref);
     }
 
     jerry_value_free(global_obj);
--- app/src/jerryzephyr/jz_helpers/jz_helpers.h
+++ app/src/jerryzephyr/jz_helpers/jz_helpers.h
@@ -15,13 +15,35 @@ int jz_object_create(char *obj_name_p);
  *
  *  @param obj_name_p string name of the object, e.g. "console"
  *  @param prop_name_p string name of the new prop, e.g. "log"
- *  @param prop_handler_p external function handler, e.g. jerryx_handler_print
+ *  @param prop_handler_p external function handler for methods, e.g. jerryx_handler_print or NULL for objects
  *
  *  @note Call jz_object_create function before this one
  *
  * @return 0 if successful, otherwise failed
  */
 int jz_object_add_prop(char *obj_name_p, char *prop_name_p, jerry_external_handler_t prop_handler_p);
+
+/**  Add an object to an existing object, should be called after jz_object_create
+ *
+ *  @param obj_name_p string name of the object, e.g. "crypto"
+ *  @param inner_obj_name_p string name of the new prop, e.g. "subtle"
+ *
+ *  @note Call jz_object_create function before this one
+ *
+ * @return 0 if successful, otherwise failed
+ */
+int jz_object_add_obj(char *obj_name_p, char *inner_obj_name_p);
+
+/** Add a new prop/method to a existing prop of an existing object
+ *
+ * @param obj_name_p string name of the object, e.g. "crypto"
+ * @param prop_name_p string name of the existing prop, e.g. "subtle"
+ * @param new_prop_name_p string name of the new prop, e.g. "digest"
+ * @param prop_handler_p external function handler, e.g. digest_handler
+ * 
+ * @return 0 if successful, otherwise failed
+ */
+int jz_object_prop_add_prop(char *obj_name_p, char *prop_name_p, char *new_prop_name_p, jerry_external_handler_t prop_handler_p);
 
 /**  Create a new function for the JerryScript machine
  *
